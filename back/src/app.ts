52139d4f20864b50c1f7810289179304249870d7
miguel bastidas <m.alejandro_80@hotmail.com>
2021-02-05 21:20:05 -0400

    commit inicial

---
+++ back/src/app.ts
@@ -0,0 +1,20 @@
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+const app = express();
+
+//settings
+app.set('port', process.env.PORT || 8000)
+
+//middlewares
+app.use(morgan('dev'))
+app.use(cors())
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+
+//routes
+
+app.get('/',(req,res)=>{res.send('todo ok')})
+
+export default app
